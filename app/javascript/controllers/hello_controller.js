22816a749dca6491cd41265bb18a8fd70c1b4c41
oLinhNguyen <nguyenquang.linh@access-company.com>
2022-10-01 13:02:52 +0700

    Call hello_controller

---
+++ app/javascript/controllers/hello_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="hello"
+export default class extends Controller {
+  static targets = [ "name", "output" ]
+
+  greet() {
+    this.outputTarget.textContent =
+      `Hello, ${this.nameTarget.value}!`
+  }
+}
